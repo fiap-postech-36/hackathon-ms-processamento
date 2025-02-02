9117bb534c8e26a09572941a7f296cc99615f7e3
leand <leandroibraimads@gmail.com>
2025-02-02 16:20:10 -0300

    feat: impl + refactor of tests for CompactVideoToZipServiceTest and NotificationServiceTest

--- application/src/test/java/br/com/videoprocessing/application/service/CompactVideoToZipServiceTest.java
+++ application/src/test/java/br/com/videoprocessing/application/service/CompactVideoToZipServiceTest.java
@@ -10,11 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.InOrder;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
-import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +22,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class CompactVideoToZipServiceTest {
 
+    // Mocks para os repositórios e o template de e-mail
     @Mock
     private VideoProcessingRepository videoProcessingRepository;
 
@@ -32,49 +32,83 @@ class CompactVideoToZipServiceTest {
     @Mock
     private MinioRepository minioRepository;
 
+    // Serviço de compactação de vídeo em ZIP
     @InjectMocks
     private CompactVideoToZipService compactVideoToZipService;
 
+    // Objeto de vídeo para teste
     private VideoProcessing videoProcessing;
+
 
     @BeforeEach
     void setUp() {
         videoProcessing = new VideoProcessing("123", "test@example.com", "video-url.mp4");
     }
 
-//    @Test
-//    void shouldProcessVideoSuccessfully() throws Exception {
-//        when(videoProcessingRepository.findById("123")).thenReturn(Optional.of(videoProcessing));
-//        when(minioRepository.downloadVideoCopy(anyString())).thenReturn(new ByteArrayInputStream(new byte[0]));
-//
-//        compactVideoToZipService.procesVideo("123");
-//
-//        verify(videoProcessingRepository, times(2)).save(any(VideoProcessing.class));
-//        verify(minioRepository, times(1)).uploadZip(any(byte[].class), anyString());
-//        verify(rabbitTemplate, times(1)).convertAndSend(eq(RabbitMQConfig.EMAIL_EXCHANGE_NAME), eq(RabbitMQConfig.EMAIL_KEY_NAME), any(EmailRabbitDTO.class));
-//    }
+    /**
+     * Testa se o serviço de compactação de vídeo em ZIP funciona corretamente.
+     */
+    @Test
+    void shouldProcessVideoSuccessfully() {
+        // Mocka o repositório de vídeo para retornar o objeto de vídeo
+        when(videoProcessingRepository.findById("123")).thenReturn(Optional.of(videoProcessing));
 
+        // Carrega um arquivo de vídeo fake do dir resources
+        String fakeVideoStreamPath = "/video-de-teste.mp4";
+        InputStream fakeVideoStream = getClass().getResourceAsStream(fakeVideoStreamPath);
+
+        // Mocka o repositório de Minio para retornar o arquivo de vídeo fake
+        when(minioRepository.downloadVideoCopy(anyString())).thenReturn(fakeVideoStream);
+
+        // Chama o serviço de compactação de vídeo em ZIP passando ID 123
+        compactVideoToZipService.procesVideo("123");
+
+        // Verifica as interações com os repositórios e o template de e-mail
+        verify(videoProcessingRepository, times(2)).save(any());
+        verify(minioRepository).uploadZip(any(), anyString());
+        verify(rabbitTemplate).convertAndSend(eq(RabbitMQConfig.EMAIL_EXCHANGE_NAME), eq(RabbitMQConfig.EMAIL_KEY_NAME), any(EmailRabbitDTO.class));
+    }
+
+    /**
+     * Testa se o serviço de compactação de vídeo em ZIP lida corretamente com falhas de processamento.
+     */
+    @Test
+    void shouldHandleProcessingFailure() {
+        // Mocka o repositório de vídeo para retornar o objeto de vídeo
+        when(videoProcessingRepository.findById("123")).thenReturn(Optional.of(videoProcessing));
+
+        // Mocka o repositório de Minio para lançar uma exceção de download
+        when(minioRepository.downloadVideoCopy(anyString())).thenThrow(new RuntimeException("Download error"));
+
+        // Verifica se o serviço de compactação de vídeo em ZIP lança uma exceção de processamento
+        assertThrows(RuntimeException.class, () -> {
+            try {
+                compactVideoToZipService.procesVideo("123");
+            } catch (Exception e) {
+                // Verifica se o template de e-mail foi enviado
+                verify(rabbitTemplate).convertAndSend(
+                        eq(RabbitMQConfig.EMAIL_EXCHANGE_NAME),
+                        eq(RabbitMQConfig.EMAIL_KEY_NAME),
+                        any(EmailRabbitDTO.class)
+                );
+                throw e;
+            }
+        });
+
+        // Verifica se o repositório de vídeo foi salvo corretamente
+        verify(videoProcessingRepository, times(2)).save(any(VideoProcessing.class));
+    }
+
+    /**
+     * Testa se o serviço de compactação de vídeo em ZIP lida corretamente com IDs de vídeo inválidos.
+     */
     @Test
     void shouldHandleInvalidVideoProcessingId() {
+        // Mocka o repositório de vídeo para retornar um objeto vazio
         when(videoProcessingRepository.findById("invalid-id")).thenReturn(Optional.empty());
 
+        // Verifica se o serviço de compactação de vídeo em ZIP lança uma exceção de ID inválido
         Exception exception = assertThrows(RuntimeException.class, () -> compactVideoToZipService.procesVideo("invalid-id"));
         assertEquals("Invalid id to procces video.", exception.getMessage());
     }
-
-//    @Test
-//    void shouldHandleProcessingFailure() throws Exception {
-//        when(videoProcessingRepository.findById("123")).thenReturn(Optional.of(videoProcessing));
-//        when(minioRepository.downloadVideoCopy(anyString())).thenThrow(new RuntimeException("Download error"));
-//
-//        assertThrows(RuntimeException.class, () -> compactVideoToZipService.procesVideo("123"));
-//
-//        InOrder inOrder = inOrder(videoProcessingRepository);
-//        inOrder.verify(videoProcessingRepository).save(argThat(v -> v.getProcessingStatus().equals("PROCESSING")));
-//        inOrder.verify(videoProcessingRepository).save(argThat(v -> v.getProcessingStatus().equals("FAILED")));
-//
-//        verify(rabbitTemplate, times(1)).convertAndSend(eq(RabbitMQConfig.EMAIL_EXCHANGE_NAME), eq(RabbitMQConfig.EMAIL_KEY_NAME), any(EmailRabbitDTO.class));
-//    }
-
-
 }
